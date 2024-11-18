3b6c509abd65297d51e9f3da91f9c0a978bbf439
Fabio Souza <fabiovanderlei.developer@gmail.com>
2024-11-18 09:23:31 -0300

    fix: add overlays

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -1,6 +1,5 @@
 # This overlay, when applied to nixpkgs, adds the final neovim derivation to nixpkgs.
-{inputs}: final: prev:
-with final.pkgs.lib; let
+{inputs}: final: prev: let
   pkgs = final;
 
   # Make sure we use the pinned nixpkgs instance for wrapNeovimUnstable,
--- plugins/default.nix
+++ plugins/default.nix
@@ -11,4 +11,4 @@
     inherit pkgs;
   };
 in
-  vimPlugins
+  vimPlugins ++ awesomeNeovimPlugins
