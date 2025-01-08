0978b4ca7659910ef8b7dc10095069fa3ec80b50
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-08 17:18:48 -0300

    chore: adjust for origin

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -1,6 +1,8 @@
 # This overlay, when applied to nixpkgs, adds the final neovim derivation to nixpkgs.
-{inputs}: final: prev: let
+{inputs}: final: prev:
+with final.pkgs.lib; let
   pkgs = final;
+
   # Make sure we use the pinned nixpkgs instance for wrapNeovimUnstable,
   # otherwise it could have an incompatible signature when applying this overlay.
   pkgs-wrapNeovim = inputs.nixpkgs.legacyPackages.${pkgs.system};
