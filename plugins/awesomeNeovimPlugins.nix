befecf05f9fdd504951e3b3a240a2d70c1677315
Fabio Souza <fabiovanderlei.developer@gmail.com>
2025-01-08 15:03:47 -0300

    fix: remove avante

--- plugins/awesomeNeovimPlugins.nix
+++ plugins/awesomeNeovimPlugins.nix
@@ -1,16 +1,15 @@
 {
   inputs,
   pkgs,
-}: let
-  # Use this to create a plugin from a flake input
-  mkNvimPlugin = import ./mkBuildVimPlugin.nix {inherit pkgs;};
-in
-  with pkgs.awesomeNeovimPlugins; [
-    # for avante
-    # (mkNvimPlugin inputs.avante-ia "avante.nvim")
-    dressing-nvim #avante-nvim
-    img-clip-nvim #avante-nvim
-    render-markdown-nvim #avante-nvim
-    # ^ for avante
-    codecompanion-nvim
-  ]
+}:
+# Use this to create a plugin from a flake input
+#mkNvimPlugin = import ./mkBuildVimPlugin.nix {inherit pkgs;};
+with pkgs.awesomeNeovimPlugins; [
+  # for avante
+  # (mkNvimPlugin inputs.avante-ia "avante.nvim")
+  dressing-nvim #avante-nvim
+  img-clip-nvim #avante-nvim
+  render-markdown-nvim #avante-nvim
+  # ^ for avante
+  codecompanion-nvim
+]
